24a236e3a1b02592c8fa709b9081c08215e6e93f
Jake Navarro <jakenavarro@hotmail.co.uk>
2025-11-03 06:55:10 +0000

    Initial commit: Student class and main in ex5.kt

---
+++ ex5.kt
@@ -0,0 +1,29 @@
+package Week_1
+
+class Student (val id: String, val name: String, val course: String, val mark: Double)
+{
+    override fun toString(): String {return "Name: $name | Course: $course | Mark: $mark"}
+}
+
+fun main() {
+    while (true) {
+        println("Enter student name (or type 'quit' to exit):\n")
+        val name = readln()
+        if (name.lowercase() == "quit") {
+            println("Exiting program...")
+            break }
+
+        println("Enter student ID:\n")
+        val id = readln()
+
+        println("Enter course:\n")
+        val course = readln()
+
+        println("Enter mark:\n")
+        val mark = readln().toDouble()
+
+        val student = Student(id, name, course, mark)
+        println("Student recorded: $student\n")
+    }
+}
+
