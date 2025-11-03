a8bf3fb8fad9be1da6606186b012f054839333d0
Jake Navarro <jakenavarro@hotmail.co.uk>
2025-11-03 07:23:32 +0000

    Added didPass function

--- ex5.kt
+++ ex5.kt
@@ -1,10 +1,14 @@
 package Week_1
 
-class Student (val id: String, val name: String, val course: String, val mark: Double)
-{
-    override fun toString(): String {return "Name: $name | Course: $course | Mark: $mark"}
-}
+class Student (val id: String, val name: String, val course: String, val mark: Double) {
+    override fun toString(): String {
+        return "Name: $name | Course: $course | Mark: $mark"
+    }
 
+    fun didPass(): Boolean {
+        return mark >= 40
+    }
+}
 fun main() {
     while (true) {
         println("Enter student name (or type 'quit' to exit):\n")
@@ -24,6 +28,7 @@ fun main() {
 
         val student = Student(id, name, course, mark)
         println("Student recorded: $student\n")
+        println("Did the student pass? ${student.didPass()}\n")
     }
 }
 
